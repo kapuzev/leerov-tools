a646f15876ae472198a2d4b168f8535d9615e341
harveyfa <ZhmulevVV@gmail.com>
2026-01-03 22:08:48 +0700

    2026-01-03 22:08:48: Autocommit
    - env.sh

--- env.sh
+++ env.sh
@@ -1,1 +1,5 @@
 export HOMEBREW_NO_AUTO_UPDATE=1
+
+export NVM_DIR="/Users/$(whoami)/goinfre/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
