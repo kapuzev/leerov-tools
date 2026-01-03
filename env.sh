c1c5fd5c039fa380e886c4d02366abdf8ae94bf1
harveyfa <ZhmulevVV@gmail.com>
2026-01-03 22:55:25 +0700

    2026-01-03 22:55:25: Autocommit
    - env.sh

--- env.sh
+++ env.sh
@@ -3,3 +3,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export NVM_DIR="/Users/$(whoami)/goinfre/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+export HOMEBREW_CASK_OPTS="--appdir=/Users/$(whoami)/goinfre/Applications"
+
+export ANDROID_NDK_HOME="/opt/goinfre/harveyfa/homebrew/share/android-ndk"
