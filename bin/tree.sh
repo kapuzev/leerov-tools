0b31cbef35a16607ae17c2d14d2756235edad378
harveyfa <ZhmulevVV@gmail.com>
2026-04-07 02:22:06 +0700

    Auto-sync 2026-04-07 02:22:06

--- bin/tree.sh
+++ bin/tree.sh
@@ -1,15 +1,16 @@
 tree() {
     local dir="${1:-.}"
     
-    # Проверяем, существует ли директория
     if [ ! -d "$dir" ]; then
         echo "Error: Directory '$dir' does not exist" >&2
         return 1
     fi
     
-    # Переходим в директорию и выводим структуру
     (
         cd "$dir" || return
-        find . -not -path '*/\.*' -print | sed -e 's;[^/]*/;│   ;g;s;│   \([^/]*$\);└── \1;'
+        find . -not -path '*/\.*' \
+               \( -name '__pycache__' -prune \) -o \
+               -not -name '*.pyc' \
+               -print | sed -e 's;[^/]*/;│   ;g;s;│   \([^/]*$\);└── \1;'
     )
 }
